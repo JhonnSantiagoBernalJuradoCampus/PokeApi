6dbdb4d9cf46d4527c6abc3626a12677386e1815
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-04-27 07:43:47 -0500

    feat: :sparkles: Worker

--- components/myModule.js
+++ components/myModule.js
@@ -1,12 +1,12 @@
 export default {
     numberStart: 0,
-    numberEnd: 10,
+    numberEnd: 1008,
   
     api(){
         const pokemonContainer = document.querySelector(".pokemon-container")
 
         const ws = new Worker("storage/wsMyModule.js", {type: "module"})
-        ws.postMessage({module: "createP", data: `https://pokeapi.co/api/v2/pokemon/?offset=${this.numberStart}&limit=${this.numberEnd}/`})
+        ws.postMessage({module: "createP", data: `https://pokeapi.co/api/v2/pokemon/?offset=${this.numberStart}&limit=${this.numberEnd}`})
         ws.addEventListener("message", (e)=>{
             let doc = new DOMParser().parseFromString(e.data, "text/html")
             pokemonContainer.append(...doc.body.children)
--- storage/wsMyModule.js
+++ storage/wsMyModule.js
@@ -1,40 +1,36 @@
 export let wsMyModule = {
     async getApi(url) {
-        try{
-            const res = await fetch(url);
-            const json = await res.json();
-            console.log(json);
-            return json
-        } catch(error){
-            console.log(`Ocurrió un error: ${error}`);
-        }
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+        console.log(json);
+        return json;
+      } catch (error) {
+        console.log(`Ocurrió un error: ${error}`);
+      }
     },
-    async createP(url){
-        let data = await this.getApi(url)
-        console.log(data.results);
-        let plantilla = "";
-        data.results.forEach(async(val,id) => {
-            console.log(val);
-            async function getPokemon(){
-                console.log(val.url);
-                let dataPokemon = await this.getApi(val.url)
-                console.log(dataPokemon);
-            }
-            getPokemon()
-            plantilla+= `
+    async createP(url) {
+        let data = await this.getApi(url);
+        let promises = data.results.map(async (val, id) => {
+          let dataPokemon = await this.getApi(`${val.url}`);
+          let template = `
             <div class="pokemon-block">
                 <div class="img-container">
-                    <img src="${val.sprites.front_default}">
+                    <img src="${dataPokemon.sprites.front_default}">
                 </div>
-                <p>#${val.id.toString().padStart(3,0)}</p>
-                <p class="name">${val.name}</p>
+                <p>#${dataPokemon.id.toString().padStart(3, 0)}</p>
+                <p class="name">${dataPokemon.name}</p>
             </div>
-            `
+        `;
+        return template;
         });
-        return plantilla
+        let pokemonData = await Promise.all(promises);
+        let template = pokemonData.join('');
+        return template;
     }
-}
-self.addEventListener("message", async (e)=>{
-    let respuesta = wsMyModule[`${e.data.module}`](e.data.data)
-    postMessage(respuesta)
-})
+        
+    }  
+self.addEventListener("message", async (e) => {
+    let respuesta = await wsMyModule[`${e.data.module}`](e.data.data);
+    postMessage(respuesta);
+});
